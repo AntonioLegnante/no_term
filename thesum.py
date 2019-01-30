45c46c3019121b663945b1c8a320dba02162dbeb
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-01-30 18:22:38 +0000

    correzioni

--- thesum.py
+++ thesum.py
@@ -4,18 +4,17 @@ file = 'file'
 
 if not os.path.isfile(file):
     print("file inesistente o altro: creazione in corso")
+    valore = 0
 else:
     f = open (file, 'r')
     valore_letto = f.readline()
-
     cast = int(valore_letto)
-
     valore = int(input ("inserisci un valore"))
     
-    somma = cast + valore
+somma = cast + valore
 
-    f.close()
+f.close()
 
-    f = open (file, 'w')
-    f.write(str(somma))
-    f.close()
+f = open (file, 'w')
+f.write(str(somma))
+f.close()
