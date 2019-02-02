21886ec2178184e53390a99dcdea1f279574513b
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-02-02 12:13:11 +0000

    linea di merda

--- thesum.py
+++ thesum.py
@@ -2,22 +2,20 @@ import os
 
 file = 'file'
 
-somma = 0
-valore = 0
-cast = 0
+
 
 if not os.path.isfile(file):
     print("file inesistente o altro: creazione in corso")
-    valore = 0
+    somma = 0
 else:
     f = open (file, 'r')
     valore_letto = f.readline()
-    cast = int(valore_letto)
+    somma = int(valore_letto)
     f.close()
 
 valore = int(input ("inserisci un valore"))
 
-somma = cast + valore
+somma += valore
 
 f = open (file, 'w')
 f.write(str(somma))
